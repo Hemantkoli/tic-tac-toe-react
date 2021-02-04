cf3d94518cc2aed912e2f66b3c0385ca57c6179e
Hemantkoli14321 <hemantkoli14321@gmail.com>
2021-02-04 13:28:41 +0530

    Create index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+                 <div className="header">Tic Tac Toe</div>
+       
+        );
+    }
+}
+
+export default Header;
